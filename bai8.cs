68f2ebcec79fcca15e1483eda504a906f5cdc40a
hunglevi93 <hung.levi93@gmail.com>
2022-04-13 13:32:50 +0700

    sum of digits

---
+++ bai8.cs
@@ -0,0 +1,31 @@
+// //tinh tong cac chu so
+// using System;
+// using System.Collections.Generic;
+
+// namespace C_BasicPratice
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Write("nhap so: ");
+//             int myNum = int.Parse(Console.ReadLine());
+//             int surplus;
+//             int sum = 0;
+//             List<int> myList = new List<int>{};
+//             while(myNum > 0)
+//             {
+//                 surplus = myNum % 10;
+//                 myList.Add(surplus);
+//                 myNum = myNum / 10;
+//             }
+//             for(int i = 0; i < myList.Count; i++)
+//             {
+//                 sum += myList[i];
+//             }
+//             Console.WriteLine(sum);
+
+//             Console.ReadKey();
+//         }
+//     }
+// }
