181d12e767042edc5aa9e1d81d92db71f800a60e
hunglevi93 <hung.levi93@gmail.com>
2022-04-13 13:27:49 +0700

    Fibonacci

---
+++ bai2.cs
@@ -0,0 +1,26 @@
+// // In dãy số Fibonacci trong C#
+// using System;
+
+// namespace bai_tap
+// {
+//     class bai2
+//     {
+//         static void Main(string[] args)
+//         {
+//             int a = 0;
+//             int b = 1;
+//             int sum = a + b;
+//             Console.Write($"{a} ");
+//             Console.Write($"{b} ");
+
+//             for(int i = 1; i <= 10; i++)
+//             { 
+//                 Console.Write($"{sum} ");
+//                 a = b;
+//                 b = sum;
+//                 sum = a + b;    
+//             }
+
+//         }
+//     }
+// }
