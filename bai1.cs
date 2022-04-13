8b249becbda7cf768c4067249b167005add06810
hunglevi93 <hung.levi93@gmail.com>
2022-04-13 13:24:39 +0700

    swap 2 int

---
+++ bai1.cs
@@ -0,0 +1,21 @@
+// // Hoan doi 2 so nguyen
+// using System;
+
+// namespace bai_tap
+// {
+//     class bai1
+//     {
+//         static void Main(string[] args)
+//         {
+//             int a = 5;
+//             int b = 8;
+//             int c;
+
+//             c = a;
+//             a = b;
+//             b = c;
+
+//             Console.WriteLine($"a = {a}, b = {b}");
+//         }
+//     }
+// }
