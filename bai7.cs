2f3730f37fd8baaf6e66474661db78c3b9fa69ef
hunglevi93 <hung.levi93@gmail.com>
2022-04-13 13:31:59 +0700

    factorial

---
+++ bai7.cs
@@ -0,0 +1,24 @@
+// // tinh giai thua cua 1 so 
+// using System;
+// using System.Collections.Generic;
+
+// namespace C_BasicPratice
+// {
+//     class bai7
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Write("nhap so: ");
+//             int myNum = int.Parse(Console.ReadLine());
+//             int volume = 1;
+
+//             for(int i = 1; i <= myNum; i++)
+//             {
+//                 volume = volume * i;
+//             }
+
+//             Console.WriteLine(volume);
+//             Console.ReadKey();
+//         }
+//     }
+// }
