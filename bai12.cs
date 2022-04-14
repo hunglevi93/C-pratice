6246dfe1e0c64ada4f83c9debe87c0c8d48fd202
hunglevi93 <hung.levi93@gmail.com>
2022-04-14 11:43:12 +0700

    delete same char in string

---
+++ bai12.cs
@@ -0,0 +1,38 @@
+// tach cac ki tu trung trong chuoi
+
+using System;
+using System.Collections.Generic;
+
+namespace C__Pratice
+{
+    class bai12
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap chuoi: ");
+            string myString = Console.ReadLine();
+            string newString = string.Empty;
+
+            for(int i = 0; i < myString.Length; i++)
+            {
+                if(!newString.Contains(myString[i]))
+                {
+                    newString += myString[i];
+                }
+                else
+                {
+                    for(int j = 0; j < newString.Length; j++)
+                    {
+                        if(newString[j] == myString[i])
+                        {
+                            newString = newString.Replace(newString[j].ToString(), string.Empty);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(newString);
+            
+            Console.ReadKey();
+        }
+    }
+}
