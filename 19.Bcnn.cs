8790c966aeffa03c9c16414e9d76238237cbbdb2
hunglevi93 <hung.levi93@gmail.com>
2022-04-16 17:01:48 +0700

    BCNN

---
+++ 19.Bcnn.cs
@@ -0,0 +1,24 @@
+// using System;
+
+// namespace C__Pratice
+// {
+//     class Bcnn
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Write("Nhap so thu nhat: ");
+//             int a = int.Parse(Console.ReadLine());
+//             Console.Write("Nhap so thu hai: ");
+//             int b = int.Parse(Console.ReadLine());
+//             for(int i = a; i >= a; i++)
+//             {
+//                 if(i%a==0 && i%b==0)
+//                 {
+//                     Console.Write($"{i} la BNCC cua {a} va {b}");
+//                     break;
+//                 }
+//             }
+//             Console.ReadKey();
+//         }
+//     }
+// }
