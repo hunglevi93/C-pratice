fcd847ecfd6a5ce591f9af4bc04c1e48229edbb1
hunglevi93 <hung.levi93@gmail.com>
2022-04-16 10:19:32 +0700

    upper first char

---
+++ 16.UpperFirstChar.cs
@@ -0,0 +1,20 @@
+//Viết hoa ký tự đầu tiên của một chữ có sẵn trong C#
+// using System;
+
+// namespace C__Pratice
+// {
+//     class UpperFirstChar
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Write("Nhap chuoi: ");
+//             string myString = Console.ReadLine();
+//             string newString = myString.Substring(1);
+//             string subString = myString.Substring(0,1).ToUpper();
+
+//             Console.WriteLine(subString + newString);
+
+//             Console.ReadKey();
+//         }
+//     }
+// }
