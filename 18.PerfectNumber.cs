98c433e4b1519f665941557b1c40c282208305d5
hunglevi93 <hung.levi93@gmail.com>
2022-04-16 16:44:04 +0700

    Perfect Number

---
+++ 18.PerfectNumber.cs
@@ -0,0 +1,34 @@
+// // Kiem tra so hoan hao
+// // So hoan la so ma tong cac uoc cua no bang chinh no
+
+// using System;
+
+// namespace C__Pratice
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Write("Nhap so: ");
+//             int myNum = int.Parse(Console.ReadLine());
+//             int sum = 0;
+//             for(int i = 1; i < myNum; i++)
+//             {
+//                 if(myNum % i == 0)
+//                 {
+//                     sum += i;
+//                 }
+//             }
+//             if(sum == myNum)
+//             {
+//                 Console.WriteLine($"{myNum} is perfect number");
+//             }
+//             else
+//             {
+//                 Console.WriteLine($"{myNum} isn't perfect number");
+//             }
+
+//             Console.ReadKey();
+//         }
+//     }
+// }
