1cc7ecd4aa84cf6e4efe2cd90b7c6d204d3b5251
hunglevi93 <hung.levi93@gmail.com>
2022-04-16 17:15:31 +0700

    Find Divisor

---
+++ 20.FindDivisor.cs
@@ -0,0 +1,27 @@
+// // Tim uoc cua 1 so
+
+// using System;
+
+// namespace C__Pratice
+// {
+//     class FindDivisor
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Write("Nhap so: ");
+//             int myNum = int.Parse(Console.ReadLine());
+//             string strNum = "";
+
+//             for(int i = 2; i < myNum; i++)
+//             {
+//                 if(myNum % i == 0)
+//                 {
+//                     strNum = strNum + i.ToString() + " ";
+//                 }
+//             }
+//             Console.WriteLine(strNum);
+
+//             Console.ReadKey();
+//         }
+//     }
+// }
